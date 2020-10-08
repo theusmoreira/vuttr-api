93e570687d44c305cf7cc988fdaf94025b18b93f
Matheus Santos <matheussm301@gmail.com>
2020-10-08 18:38:05 -0300

    feat: Add in server expeception handler

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
 import 'reflect-metadata';
 import './database';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-// eslint-disable-next-line no-console
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(400).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
 app.listen(3333, () => console.log('🚀 Server started on port 3333!'));
