3170bd32daa237e0dfe008d9fb3c8a9fd28d3e95
Matheus Santos <matheussm301@gmail.com>
2020-10-08 18:40:07 -0300

    chore: Add sessions router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import toolsRouter from './tools.routes';
 import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/tools', toolsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
