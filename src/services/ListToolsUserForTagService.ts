fcb0f398fc74e01cea543055784f836963ca44cd
Matheus Santos <matheussm301@gmail.com>
2020-10-08 23:55:31 -0300

    feat: Create list tools for tag

---
+++ src/services/ListToolsUserForTagService.ts
@@ -0,0 +1,22 @@
+import { getCustomRepository } from 'typeorm';
+import Tool from '../database/entities/Tool';
+import ToolRepository from '../repositories/ToolsRepository';
+
+interface IRequest {
+  tag: string;
+  user_id: string;
+}
+
+class ListToolsUserService {
+  public async execute({ user_id, tag }: IRequest): Promise<Tool[]> {
+    const toolRepository = getCustomRepository(ToolRepository);
+
+    const tools = await toolRepository.find({ where: { user_id } });
+
+    const findToolsforTag = tools.filter(tool => tool.tags.includes(tag));
+
+    return findToolsforTag;
+  }
+}
+
+export default ListToolsUserService;
