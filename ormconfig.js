0c5e434e232ba2eacd5a0c4ebf8940c78410c8a9
Matheus Santos <matheussm301@gmail.com>
2020-10-20 23:09:40 -0300

    feat: Add .env

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,14 @@
-module.exports = {
+require("dotenv/config");
+
+const devConfig = [
+  {
     name: 'default',
     type: 'postgres',
-    host: "localhost",
+    host: process.env.POSTGRES_HOST,
     port: 5432,
-    username: "postgres",
-    password: "docker",
-    database: "vuttr",
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    database: process.env.POSTGRES_DB_NAME,
     entities: [
       './src/modules/**/infra/typeorm/entities/*.ts'
     ],
@@ -15,4 +18,29 @@ module.exports = {
     cli: {
       migrationsDir: './src/shared/infra/typeorm/migrations',
     },
-}
+  },
+];
+
+const prodConfig = [
+  {
+    name: 'default',
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    database: process.env.POSTGRES_DB_NAME,
+    entities: [
+      './dist/modules/**/infra/typeorm/entities/*.js'
+    ],
+    migrations: [
+      './dist/shared/infra/typeorm/migrations/*.js'
+    ],
+    cli: {
+      migrationsDir: './dist/shared/infra/typeorm/migrations',
+    },
+  },
+
+];
+
+module.exports = process.env.NODE_ENV === 'development' ? devConfig : prodConfig;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,12 +5,14 @@ import '@shared/infra/typeorm';
 import '@shared/container';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import { errors } from 'celebrate';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors());
